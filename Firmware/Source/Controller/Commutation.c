d689abe8a3e51aee25d963612fabc0be44116c9f
asytyi <alexander.a.sytyi@gmail.com>
2020-07-14 16:52:53 +0300

    Пример реализации функций коммутации

--- Firmware/Source/Controller/Commutation.c
+++ Firmware/Source/Controller/Commutation.c
@@ -9,6 +9,89 @@ static Int8U COMM_NewOutputValues[SUM_OF_ALL_BYTE_REGISTERS];
 static Int8U COMM_CurrentOutputValues[SUM_OF_ALL_BYTE_REGISTERS];
 static Int8U COMM_TransferOutputValues[SUM_OF_ALL_BYTE_REGISTERS];
 
+
+static uint8_t ShiftRegistersState[REGISTERS_NUM] = {0};
+static uint8_t BistableBits[REGISTERS_NUM] = {0};
+
+void COMM_SwitchSimpleDevice(RegisterPin Device, bool State);
+void COMM_SwitchBistableDevice(BistableSwitch Device, bool State);
+void COMM_ApplyCommutation();
+void COMM_DisconnectBistable();
+
+// С помощью этой функции задаём состояние для простых реле на сдвиговом регистре
+void COMM_SwitchSimpleDevice(RegisterPin Device, bool State)
+{
+	if(State)
+		ShiftRegistersState[Device.Register] |= Device.Bit;
+	else
+		ShiftRegistersState[Device.Register] &= ~Device.Bit;
+}
+// ----------------------------------------
+
+// С помощью этой функции задаём состояние для бистабильного коммутатора,
+// состоящего из двух бистабильных реле
+void COMM_SwitchBistableDevice(BistableSwitch Device, bool State)
+{
+	if(State)
+	{
+		// Уставка битов при включении
+		ShiftRegistersState[Device.TurnOn1.Register] |= Device.TurnOn1.Bit;
+		ShiftRegistersState[Device.TurnOn2.Register] |= Device.TurnOn2.Bit;
+
+		// Отдельно запоминаем эти биты
+		BistableBits[Device.TurnOn1.Register] |= Device.TurnOn1.Bit;
+		BistableBits[Device.TurnOn2.Register] |= Device.TurnOn2.Bit;
+	}
+	else
+	{
+		// Уставка битов при выключении
+		ShiftRegistersState[Device.TurnOff1.Register] |= Device.TurnOff1.Bit;
+		ShiftRegistersState[Device.TurnOff2.Register] |= Device.TurnOff2.Bit;
+
+		// Отдельно запоминаем эти биты
+		BistableBits[Device.TurnOff1.Register] |= Device.TurnOff1.Bit;
+		BistableBits[Device.TurnOff2.Register] |= Device.TurnOff2.Bit;
+	}
+}
+// ----------------------------------------
+
+// Пример выполнения коммутации
+void COMM_Examle()
+{
+	// Формируем битовые значения для сдвиговых регистров и запоминаем бистабильные реле
+	COMM_SwitchSimpleDevice(COMM_SwitchPOTPlusToPOT1, true);
+	COMM_SwitchBistableDevice(COMM_SwitchBUSLVPlusToPOW1, true);
+
+	// Отправляем уставку в сдвиговые регистры
+	COMM_ApplyCommutation();
+
+	// Задержка
+
+	// Отдельно отключаем бистабильные реле
+	COMM_DisconnectBistable();
+}
+// ----------------------------------------
+
+void COMM_ApplyCommutation()
+{
+	// Здесь идёт запись массива ShiftRegistersState[] по SPI
+}
+// ----------------------------------------
+
+void COMM_DisconnectBistable()
+{
+	// Мы запомнили биты, относящиеся к бистабильным реле
+	// Теперь их отключаем и очищаем
+	for(uint8_t i = 0; i < REGISTERS_NUM; i++)
+	{
+		ShiftRegistersState[i] &= BistableBits[i];
+		BistableBits[i] = 0;
+	}
+
+	COMM_ApplyCommutation();
+}
+// ----------------------------------------
+
 // Function
 void COMM_ConnectGroup(Int8U Group)
 {
--- Firmware/Source/Controller/Commutation.h
+++ Firmware/Source/Controller/Commutation.h
@@ -4,6 +4,44 @@
 #include "stdinc.h"
 #include "ZwSPI.h"
 
+
+#define REGISTERS_NUM		11
+
+#define REGISTER_A			8
+#define REGISTER_B			9
+#define REGISTER_C			10
+#define REGISTER_D			2
+#define REGISTER_E			3
+#define REGISTER_F			0
+#define REGISTER_G			1
+#define REGISTER_H			7
+#define REGISTER_I			6
+#define REGISTER_J			5
+#define REGISTER_K			4
+
+typedef struct __RegisterPin
+{
+	uint8_t Register;
+	uint8_t Bit;
+} RegisterPin;
+#define COMM_RegisterPin static const RegisterPin
+
+typedef struct __BistableSwitch
+{
+	RegisterPin TurnOn1;
+	RegisterPin TurnOn2;
+	RegisterPin TurnOff1;
+	RegisterPin TurnOff2;
+} BistableSwitch;
+#define COMM_BistableSwitch static const BistableSwitch
+
+// Пример инициализации для простого реле
+COMM_RegisterPin COMM_SwitchPOTPlusToPOT1 = {REGISTER_A, BIT0};
+
+// Пример инициализации бистабильного ключа
+COMM_BistableSwitch COMM_SwitchBUSLVPlusToPOW1 = {{REGISTER_A, BIT1}, {REGISTER_A, BIT2}, {REGISTER_A, BIT3}, {REGISTER_A, BIT4}};
+
+
 // Macros
 //
 #define TYPE_RELAY_NORMAL		0
